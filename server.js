ec6d217b8878d2b35a3239b17623296052afe153
mukesh murugan <hello@mukeshmurugan.com>
2020-01-13 22:27:52 +0530

    something

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-   res.send("Hello world!");
+   res.send("Hello world! First End Point");
 });
 
 app.listen(1994);
