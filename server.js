f293e3534a701ee2a118b6a0cd3ccb1cefb9c22b
mukesh murugan <hello@mukeshmurugan.com>
2020-01-13 22:34:21 +0530

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-   res.send("Hello world! First End Point");
+   res.send("Hello world! First End Point works!");
 });
 
 app.listen(1994);
